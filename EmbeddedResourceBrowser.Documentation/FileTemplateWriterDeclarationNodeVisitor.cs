89b91059ec5f536a65fcdf6ad0ac06dcc159741e
Andrei Fangli <andrei_fangli@outlook.com>
2020-10-31 14:48:18 +0200

    Moved project site to docs folder

--- EmbeddedResourceBrowser.Documentation/FileTemplateWriterDeclarationNodeVisitor.cs
+++ EmbeddedResourceBrowser.Documentation/FileTemplateWriterDeclarationNodeVisitor.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using CodeMap.DeclarationNodes;
 using CodeMap.Handlebars;
@@ -8,12 +7,16 @@ namespace EmbeddedResourceBrowser.Documentation
     public class FileTemplateWriterDeclarationNodeVisitor : TemplateWriterDeclarationNodeVisitor
     {
         private readonly IMemberReferenceResolver _memberFileNameResolver;
+        private readonly DirectoryInfo _outputDirectory;
 
-        public FileTemplateWriterDeclarationNodeVisitor(IMemberReferenceResolver memberFileNameResolver, TemplateWriter templateWriter)
+        public FileTemplateWriterDeclarationNodeVisitor(DirectoryInfo outputDirectory, IMemberReferenceResolver memberFileNameResolver, TemplateWriter templateWriter)
             : base(templateWriter)
-            => _memberFileNameResolver = memberFileNameResolver;
+        {
+            _memberFileNameResolver = memberFileNameResolver;
+            _outputDirectory = outputDirectory;
+        }
 
         protected override TextWriter GetTextWriter(DeclarationNode declarationNode)
-            => new StreamWriter(new FileStream(Path.Combine(Environment.CurrentDirectory, _memberFileNameResolver.GetFileName(declarationNode)), FileMode.Create, FileAccess.Write, FileShare.Read));
+            => new StreamWriter(new FileStream(Path.Combine(_outputDirectory.FullName, _memberFileNameResolver.GetFileName(declarationNode)), FileMode.Create, FileAccess.Write, FileShare.Read));
     }
 }
--- EmbeddedResourceBrowser.Documentation/Program.cs
+++ EmbeddedResourceBrowser.Documentation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using CodeMap.DeclarationNodes;
 using CodeMap.Handlebars;
 
@@ -7,17 +8,30 @@ namespace EmbeddedResourceBrowser.Documentation
 {
     internal static class Program
     {
-        internal static void Main()
+        internal static void Main(params string[] args)
         {
+            if (args.Length == 0)
+                throw new ArgumentException("Expected output directory path as first argument.");
+
+            var outputDirectory = new DirectoryInfo(Path.Combine(Environment.CurrentDirectory, args.First()));
+            outputDirectory.Create();
+            _ClearFolder(outputDirectory);
+
             var embeddedResourceBrowserAssembly = typeof(EmbeddedDirectory).Assembly;
             var memberReferenceResolver = new DefaultMemberReferenceResolver(embeddedResourceBrowserAssembly, "netstandard-1.6");
             var templateWriter = new EmbeddedResourceBrowserHandlebarsTemplateWriter(memberReferenceResolver);
-            var nodeVisitor = new FileTemplateWriterDeclarationNodeVisitor(memberReferenceResolver, templateWriter);
+            var nodeVisitor = new FileTemplateWriterDeclarationNodeVisitor(outputDirectory, memberReferenceResolver, templateWriter);
 
             DeclarationNode.Create(embeddedResourceBrowserAssembly).Apply(new DocumentationAdditon()).Accept(nodeVisitor);
 
             var assets = new EmbeddedDirectory(typeof(Program).Assembly).Subdirectories["Assets"];
-            assets.CopyTo(new DirectoryInfo(Environment.CurrentDirectory));
+            assets.CopyTo(outputDirectory);
+        }
+
+        private static void _ClearFolder(DirectoryInfo directory)
+        {
+            foreach (var file in directory.GetFiles())
+                file.Delete();
         }
     }
 }
